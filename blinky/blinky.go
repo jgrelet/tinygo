262ae910a47a024628f12a41fc6617605020548c
Jacques Grelet <jacques.grelet@gmail.cpm>
2025-03-22 20:59:43 +0100

    machine.LED not working, use GP0 instead

--- blinky/blinky.go
+++ blinky/blinky.go
@@ -1,24 +1,32 @@
 // blinky.go
 
-// tinygo flash -target=pico-w ./main.go
+// tinygo flash -target=pico-w -monitor ./main.go
+// or
+// make run
 
 package main
 
 import (
 	"machine"
 	"time"
+	"fmt"
 )
 
 func main() {
-	led := machine.LED
+	time.Sleep(time.Second)
+	println("Start blinking")
+	//led := machine.LED
+	led := machine.GP0 // pin 1
 	led.Configure(machine.PinConfig{
 		Mode: machine.PinOutput,
 	})
 
 	for {
 		led.High()
-		time.Sleep(500 * time.Millisecond)
+		fmt.Printf("Led on: %t\n", led.Get())
+		time.Sleep(1 * time.Second)
 		led.Low()
-		time.Sleep(500 * time.Millisecond)
+		fmt.Printf("Led off: %t\n", led.Get())
+		time.Sleep(1 * time.Second)
 	}
 }
