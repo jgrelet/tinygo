9c226df154491a597de28f5e060fff6c7b74b259
Jacques Grelet <jacques.grelet@gmail.cpm>
2025-03-23 00:04:08 +0100

    use  ssd1306_font lib to display text

--- ssd1306/go.mod
+++ ssd1306/go.mod
@@ -2,6 +2,9 @@ module tinygo/ssd106
 
 go 1.24.1
 
-require tinygo.org/x/drivers v0.31.0
+require (
+	github.com/Nondzu/ssd1306_font v1.0.1
+	tinygo.org/x/drivers v0.31.0
+)
 
 require github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
--- ssd1306/main.go
+++ ssd1306/main.go
@@ -1,67 +1,39 @@
-// This example shows how to use 128x64 display over I2C
-// Tested on Seeeduino XIAO Expansion Board https://wiki.seeedstudio.com/Seeeduino-XIAO-Expansion-Board/
-//
-// According to manual, I2C address of the display is 0x78, but that's 8-bit address.
-// TinyGo operates on 7-bit addresses and respective 7-bit address would be 0x3C, which we use below.
-//
-// To learn more about different types of I2C addresses, please see following page
-// https://www.totalphase.com/support/articles/200349176-7-bit-8-bit-and-10-bit-I2C-Slave-Addressing
+//from https://github.com/Nondzu/ssd1306_font
 
 package main
 
 import (
 	"machine"
-
-	_ "image/color"
 	"time"
 
+	font "github.com/Nondzu/ssd1306_font"
 	"tinygo.org/x/drivers/ssd1306"
 )
 
 func main() {
-	time.Sleep(1 * time.Second)
-	println("Start Oled display")
 
-	machine.I2C1.Configure(machine.I2CConfig{
-		Frequency: 100 * machine.KHz,
-	})
+	time.Sleep(time.Second)
+	println("Start Oled display") // Please wait some time after turning on the device to properly initialize the display
+	machine.I2C1.Configure(machine.I2CConfig{Frequency: 400000})
 
-	display := ssd1306.NewI2C(machine.I2C1)
-	display.Configure(ssd1306.Config{
-		Address: 0x3C,
-		Width:   128,
-		Height:  64,
-	})
+	// Display
+	dev := ssd1306.NewI2C(machine.I2C1)
+	dev.Configure(ssd1306.Config{Width: 128, Height: 64, Address: 0x3C, VccState: ssd1306.SWITCHCAPVCC})
+	dev.ClearBuffer()
+	dev.ClearDisplay()
 
-	display.ClearBuffer()
-	display.ClearDisplay()
-	display.Tx([]byte("Hello, world!"), true)
-	display.Display()
-/*
-	x := int16(0)
-	y := int16(0)
-	deltaX := int16(1)
-	deltaY := int16(1)
+	//font library init
+	display := font.NewDisplay(dev)
+	display.Configure(font.Config{FontType: font.FONT_7x10}) //set font here
+
+	display.YPos = 0                 // set position Y
+	display.XPos = 0                  // set position X
+	display.PrintText("Hello World!") // print text
+	display.YPos = 20                // set position Y
+	display.XPos = 0                  // set position X
+	display.PrintText("Temp = 20 C") // print text
+
 	for {
-		pixel := display.GetPixel(x, y)
-		c := color.RGBA{255, 255, 255, 255}
-		if pixel {
-			c = color.RGBA{0, 0, 0, 255}
-		}
-		display.SetPixel(x, y, c)
-		display.Display()
 
-		x += deltaX
-		y += deltaY
-
-		if x == 0 || x == 127 {
-			deltaX = -deltaX
-		}
-
-		if y == 0 || y == 63 {
-			deltaY = -deltaY
-		}
-		time.Sleep(1 * time.Millisecond)
 	}
-		*/
 }
