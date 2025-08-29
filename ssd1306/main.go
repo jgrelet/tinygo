7c6563434c0f2e4f7cf8072c8074b872e3785578
Jacques Grelet <jacques.grelet@gmail.cpm>
2025-08-29 15:56:05 +0200

    Change freq to 400khz

--- ssd1306/main.go
+++ ssd1306/main.go
@@ -15,9 +15,10 @@ func main() {
 	time.Sleep(time.Second)
 	println("Start Oled display") // Please wait some time after turning on the device to properly initialize the display
 	machine.I2C1.Configure(machine.I2CConfig{
-		Frequency: 100 * machine.KHz,
+		//Frequency: 400000,
+		Frequency: 400 * machine.KHz,
 		SCL: machine.I2C1_SCL_PIN,
-		SDA: machine.I2C1_SDA_PIN
+		SDA: machine.I2C1_SDA_PIN,
 	})
 	
 	//machine.I2C1.Configure(machine.I2CConfig{Frequency: 400000})
@@ -30,7 +31,7 @@ func main() {
 
 	//font library init
 	display := font.NewDisplay(dev)
-	display.Configure(font.Config{FontType: font.FONT_6x8}) //set font here
+	display.Configure(font.Config{FontType: font.FONT_7x10}) //set font here
 
 	display.YPos = 0                 // set position Y
 	display.XPos = 0                  // set position X
@@ -40,10 +41,8 @@ func main() {
 	display.PrintText("Temp = 20 C") // print text
 	display.YPos = 24               // set position Y
 	display.XPos = 0                  // set position X
-	display.PrintText("Pres = 1023 mb") // print text
-
+	display.PrintText("Pres = 1023 mb") // print text      
 
 	for {
-
 	}
 }
